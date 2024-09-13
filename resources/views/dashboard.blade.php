4f83523cf0875c8d89b3b7c0e9640e35ec79d504
Deepthi35 <deepthimadhabattula@gmail.com>
2024-09-13 21:06:27 +0530

    initial setup for liveware tables

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<p>This is Dashboard</p>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <h1 class="text-black-50">welcome Home Admin</h1>
+    </div>
+@endsection
